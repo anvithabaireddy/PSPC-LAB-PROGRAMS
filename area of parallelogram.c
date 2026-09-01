cff4df7f25179917a472dd84e5ad9bc1cc9a7323
Anvitha Baireddy <anvithabaireddy@gmail.com>
2026-09-02 01:48:50 +0530

    Add files via upload

---
+++ area of parallelogram.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float b,h,area;
+	printf("Enter base and height of parallelogram\n");
+	scanf("%f%f",&b,&h);
+	area=b*h;
+	printf("area of parallelogram=%f\n",area);
+	return 0;
+}
